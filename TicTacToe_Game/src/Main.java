11bc60aac044013439f1792b80bcdcc354ccb394
Gergo Szabo <gergo.szabo23@gmail.com>
2017-08-15 12:48:05 +0200

    Print board

--- TicTacToe_Game/src/Main.java
+++ TicTacToe_Game/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.print("Hello");
+        tictactoe tic = new tictactoe();
+
+        tic.printBoard();
 
     }
 }
--- TicTacToe_Game/src/tictactoe.java
+++ TicTacToe_Game/src/tictactoe.java
@@ -1,3 +1,44 @@
 public class tictactoe extends Main{
 
+    private char[][] board;
+    private char playerMark;
+
+    public tictactoe() {
+        board = new char[3][3];
+        playerMark = 'x';
+        emptyBoardTable();
+    }
+
+    public void emptyBoardTable() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '#';
+
+            }
+        }
+    }
+
+    public void printBoard() {
+
+        System.out.println("-------------");
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.print("| ");
+
+            for (int j = 0; j < 3; j++) {
+
+                System.out.print(board[i][j] + "| ");
+            }
+
+            System.out.println();
+            System.out.println("-------------");
+
+
+        }
+
+    }
+
+
+
 }
