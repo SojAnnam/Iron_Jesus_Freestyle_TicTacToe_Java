62ddb81dfe57afce5a07c82585bdd4f41ece0266
Gergo Szabo <gergo.szabo23@gmail.com>
2017-08-17 14:05:10 +0200

    Auto stash before merge of "development" and "origin/development"
    fixing

--- TicTacToe_Game/src/Main.java
+++ TicTacToe_Game/src/Main.java
@@ -7,23 +7,30 @@ public class Main {
 
         ticTacToe tic = new ticTacToe();
 
-        while (tic.boardIsFull != true) {
+        while (tic.boardIsFull != true && tic.winCheck() == 'a') {
             tic.printBoard();
             System.out.println(tic.boardFull());
+            System.out.println(tic.winCheck());
             List returnList = tic.userInputs();
             tic.placeChar(returnList);
             System.out.println(tic.playerChar);
             tic.boardFull();
+
+
+        }
+
+        if (tic.boardIsFull = true && tic.winCheck() != 'a') {
+
+
+            tic.printBoard();
             if (tic.winCheck() == 'x') {
                 System.out.println("Player One won.");
             } else if (tic.winCheck() == 'o'){
                 System.out.println("Player Two won.");
             }
-        }
-
-        if (tic.boardIsFull = true) {
-
+            System.out.println(tic.winCheck());
             System.out.println("The match ends in a draw!");
+
         }
     }
 }
